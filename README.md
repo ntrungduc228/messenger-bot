f08c06ea083f99d356029e79818297b7bfebfe0a
Duc Nguyen <ntrungduc228@gmail.com>
2021-07-17 15:34:25 +0700

    create class controller

---
+++ README.md
@@ -0,0 +1,10 @@
+# Facebook messenger chatbot
+
+## This chatbot integrates:
+
+## Menu
+
+Simsimi
+filter girl's image (j2team girl)
+sleeping time
+food (angiday)
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,14 @@
-const chatbotRouter = require('./chatbot');
+const express = require('express');
+let router = express.Router();
+const homeController = require('../controllers/homeController');
 
 function route(app) {
-    app.use('/', chatbotRouter);
+
+    router.get('/webhook', homeController.getWebhook);
+    router.post('/webhook', homeController.postWebhook);
+    router.get('/', homeController.getHomePage);
+
+    app.use('/', router);
 }
 
 module.exports = route;

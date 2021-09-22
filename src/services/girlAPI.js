4d243ff263f59e462917ae9a37a6d082d4e1b486
Duc Nguyen <ntrungduc228@gmail.com>
2021-09-23 06:51:10 +0700

    Update Sim & Girl API

--- src/services/girlAPI.js
+++ src/services/girlAPI.js
@@ -1,30 +1,32 @@
 require("dotenv").config();
 const request = require("request");
-class GirlAPI{
+class GirlAPI {
+  getRandomGirlImage() {
+    const maxIndex = 37000;
+    const perPage = 50;
+    // const pageIndex = Math.floor(Math.random() * Math.floor(maxIndex/perPage));
+    const pageIndex = Math.floor(Math.random() * 230);
+    const randomIndex = Math.floor(Math.random() * perPage);
+    console.log("randomIndex", randomIndex, "pageIndex", pageIndex);
+    return new Promise((resolve, reject) => {
+      request(
+        {
+          url: `https://api.huynq.net/sosexy.php?api_key=28282300029828811123&api_user=j2team&app=gallery&page=${pageIndex}`,
+          method: "GET",
+        },
+        (err, response, body) => {
+          if (err) {
+            reject(err);
+          }
 
-    getRandomGirlImage(){
-        const maxIndex = 37000;
-        const perPage = 50;
-        const pageIndex = Math.floor(Math.random() * Math.floor(maxIndex/perPage));
-        const randomIndex = Math.floor(Math.random() * perPage);
-        console.log('randomIndex', randomIndex, 'pageIndex', pageIndex);
-        return new Promise((resolve, reject) => {
-            request({
-                url: `https://api.huynq.net/sosexy.php?api_key=28282300029828811123&api_user=j2team&app=gallery&page=${pageIndex}`,
-                method: 'GET'
-            }, (err, response, body) => {
-                if(err){
-                    reject(err);
-                }
-
-                var rs = JSON.parse(body);
-                //var imageURL = rs; 
-                var imageURL =  rs[1].data.photos[randomIndex].original.url;
-                resolve(imageURL)
-            })
-        })
-    }
-
+          var rs = JSON.parse(body);
+          //var imageURL = rs;
+          var imageURL = rs[1].data.photos[randomIndex].original.url;
+          resolve(imageURL);
+        }
+      );
+    });
+  }
 }
 
 module.exports = new GirlAPI();
--- src/services/simAPI.js
+++ src/services/simAPI.js
@@ -1,28 +1,30 @@
 require("dotenv").config();
 const request = require("request");
 
-class SimsimiAPI{
-
-    getMessage(text){
-        return new Promise((resolve, reject) => {
-            request({
-                url: `https://api.simsimi.net/v1/?text=${text}&lang=vi_VN`,
-                method: "GET",
-            }, (err, res, body) => {
-                if(err){
-                    reject(err);
-                }
-                var rs = {};
-                try{
-                  rs = JSON.parse(body);
-                }
-                catch(err){ console.log(err); 
-                  rs.success = "BOT ốm rùi không nói chuyện với bạn được. Sorry !!!"
-                }
-                resolve(rs.success);
-            });
-        });
-    }
+class SimsimiAPI {
+  getMessage(text) {
+    return new Promise((resolve, reject) => {
+      request(
+        {
+          url: `https://api.simsimi.net/v2/?text=${text}&lang=vi`,
+          method: "GET",
+        },
+        (err, res, body) => {
+          if (err) {
+            reject(err);
+          }
+          var rs = {};
+          try {
+            rs = JSON.parse(body);
+          } catch (err) {
+            console.log(err);
+            rs.success = "BOT ốm rùi không nói chuyện với bạn được. Sorry !!!";
+          }
+          resolve(rs.success);
+        }
+      );
+    });
+  }
 }
 
 module.exports = new SimsimiAPI();

d5dbbe1ce5c4d5b6257a6b3268acca59e3fc3f8a
Duc Nguyen <ntrungduc228@gmail.com>
2021-09-24 16:55:11 +0700

    Update Sim API

--- src/services/simAPI.js
+++ src/services/simAPI.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const request = require("request");
 
+// API Resource: https://www.simsimi.net/#usages
+
 class SimsimiAPI {
   getMessage(text) {
     return new Promise((resolve, reject) => {
       request(
         {
-          url: `https://api.simsimi.net/v2/?text=${text}&lang=vi`,
+          url: `https://api.simsimi.net/v2/?text=${text}&lc=vn&cf=false`,
           method: "GET",
         },
         (err, res, body) => {
